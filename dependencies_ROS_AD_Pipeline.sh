90a580e4da2430d072881c88fd8fee04a58e539d
Carlos <cconejob@gmail.com>
2021-01-15 15:52:41 +0100

    update README file

--- dependencies_ROS_AD_Pipeline.sh
+++ dependencies_ROS_AD_Pipeline.sh
@@ -1,56 +1,15 @@
 #!/bin/bash
 
 # PYTHON 3.7
-
-# build a docker
-docker build --tag python_builder .
-docker run --rm --interactive --tty --name python_builder python_builder
-
-# run inside the docker container:
-set -o errexit -o nounset -o pipefail -o xtrace
-
-PYTHON_VERSION=3.7.6
-
-# this must not be plain python as otherwise the system Python gets overridden
-PACKAGE_NAME=python3.7
-
-# sources need to be available for build-deb
-sed --in-place=~orig 's/# deb-src/deb-src/' /etc/apt/sources.list
-
-apt-get update
-apt-get upgrade
-
-# install things we need for this script
-apt-get install --assume-yes \
-    checkinstall \
-    wget \
-
-# install things to compile Python
-apt-get build-dep --assume-yes python3.5
-
-# download the latest version (from https://www.python.org/downloads/source/)
-wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
-
-tar xvf Python-${PYTHON_VERSION}.tgz
-cd Python-${PYTHON_VERSION}
-
-# enabling optimizations requires running tests which takes a long time but results in a faster Python
-./configure \
-    --enable-optimizations \
-    --with-ensurepip=install \
-
-# compile as much as possible in parallel
-make --jobs 4
-
-# use altinstall to not replace the system Python
-checkinstall --pkgname ${PACKAGE_NAME} --pkgversion ${PYTHON_VERSION} --default make altinstall
-
-# run this from a another terminal while the container is still running:
-docker cp python_builder:/Python-3.7.2/python3.7_3.7.2-1_amd64.deb .
-
-# install the package
-sudo dpkg -i python3.7_3.7.2-1_amd64.deb
-
+sudo apt update
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+cd /tmp
+wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tar.xz
+tar -xf Python-3.7.2.tar.xz
+cd Python-3.7.2
+./configure --enable-optimizations
+make -j 1
+sudo make altinstall
 
 # ROS KINETIC
 
@@ -185,11 +144,11 @@ source trajnetv/bin/activate
 
 ## Download Requirements
 cd trajnetplusplusbaselines/ 
-pip install -e .
+sudo -H pip install -e .
 
 cd ../trajnetplusplusdataset/ 
-pip install -e .
-pip install -e '.[test, plot]'
+sudo -H pip install -e .
+sudo -H pip install -e '.[test, plot]'
 
 
 # Other common modules
