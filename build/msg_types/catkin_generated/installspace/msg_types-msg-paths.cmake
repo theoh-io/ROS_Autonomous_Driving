f8a5be6ab1710054e06e97c3ecd3c17c5e6b57b8
Carlos <cconejob@gmail.com>
2021-02-16 14:54:45 +0100

    CHUV

--- build/msg_types/catkin_generated/installspace/msg_types-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
-
-_prepend_path("${msg_types_DIR}/.." "msg" msg_types_MSG_INCLUDE_DIRS UNIQUE)
-set(msg_types_MSG_DEPENDENCIES std_msgs;geometry_msgs;msg_types)
--- build/state_estimation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cconejob/StudioProjects/Autonomous_driving_pipeline/src/state_estimation
-# Build directory: /home/cconejob/StudioProjects/Autonomous_driving_pipeline/build/state_estimation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
