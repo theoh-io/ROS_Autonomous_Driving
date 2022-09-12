d0d53ef48d5e5c6ff92b7ba1bea24faf719120de
Theo Hermann <theo.hermann@epfl.ch>
2022-09-12 15:16:32 +0200

    updated install procedure

--- README.md
+++ README.md
@@ -12,11 +12,116 @@ Before adopting the pipeline for a singular purpose, we strongly recommend to re
 
 ### Quick Start
 
-First of all, we need to clone all the ```./src``` folder inside a new ROS folder:
+
+
+### Structure
+
+We present all the different pillars of the pipeline and a brief explanation of each one.
+
+```
+Autonomous Pipeline
+│
+│─── Loomo ───> Package which contains the launch file with all configurable parameters.
+│
+│─── Message Types ───> Package which includes all types of messages for topics.
+│
+│─── Perception ───> Package and node to robustly detect and track from cameras and depth sensors.
+│
+│─── Estimation ───> Package to estimate current state from sensors information.
+│       │
+│       │─── Robot State ───> Node to estimate the state from IMU and GPS.
+│       │
+│       └─── Map State ───> Node to generate a map and estimate the state from all sensors.
+│
+│─── Prediction ───> Package and node to generate trajectory prediction of the detected person.
+│
+│─── Path Planning ───> Package and node to calculate robot's desired path to avoid collision.
+│
+│─── Control ───> Package and node to ensure that the robot is following the desired path.
+│
+└─── Visualization ───> Plotting tools for all nodes current data.
+```
+---
+## Install
+
+Grab a cup of coffee :coffee: and get ready for the full installation procedure. This is a long and quite tedious process, take your time and follow each step carrefully. Everything is going to be fine :relieved:.
+
+First step is to clone the repository inside the desired ROS folder using:
+
+    git clone https://github.com/theoh-io/ROS_Autonomous_Driving
+
+### Dependencies
+
+**Python 3.7**
+
+Check if python 3.7 is already installed running python3.7 in terminal.
+If you do not have this Python version, we recommend you to download it by typing the following command in the Ubuntu terminal:
 
 ```shell
-git clone https://github.com/cconejob/Autonomous_driving_pipeline.git
+sudo chmod +x python_Autonomous_driving_pipeline.sh
+./python_Autonomous_driving_pipeline.sh
 ```
+
+**ROS Kinetic/Melodic/Noetic**
+
+Depending on the Ubuntu version, there are three compatible ROS distributions.
+
+* Ubuntu 16.04: ROS Kinetic. http://wiki.ros.org/kinetic/Installation/Ubuntu
+
+* Ubuntu 18.04: ROS Melodic. http://wiki.ros.org/melodic/Installation/Ubuntu
+
+* Ubuntu 20.04: ROS Noetic. http://wiki.ros.org/noetic/Installation/Ubuntu
+
+Check installed Version by using `rosversion -d ` in terminal
+
+**Python Libraries**
+
+First create a virtual environment using python 3.7 by typing `python3.7 -m venv <name of your virtual environment>` and then activate it using `source <name venv>/bin/activate`. 
+
+Finally we advise to update pip and basic package using `python -m pip install -U pip wheel setuptools --no-cache-dir `
+
+We have built a bash script and a requirements.txt file with all the python modules required for our ROS pipeline. You can run them in your command line by typing:
+
+:warning: this step might cause you some trouble, if the scripts keeps throwing error messages try to run it line by line and identifying the command causing problem. If it persists feel free to create an issue in the repo.
+
+```shell
+sudo chmod +x dependencies_Autonomous_driving_pipeline.sh
+./dependencies_Autonomous_driving_pipeline.sh
+pip install -r requirements.txt
+```
+
+**TrajNet++**
+
+Follow the installation procedure described in the [trajnetbaseline](https://github.com/vita-epfl/trajnetplusplusbaselines) repo. and this step by step [guide](https://thedebugger811.github.io/posts/2020/03/intro_trajnetpp/).
+
+[trajnetplusplusdataset]()
+[ẗrajnetplusplustools]()
+
+
+Indicative list of dependencies:
+* Openpifpaf 0.11.9
+
+* TrajNet++
+
+* math
+
+* numpy
+
+* scipy
+
+* matplotlib
+
+* PIL
+
+* torch 1.7.1
+
+* collections
+
+* cython 0.29.21 
+
+* OpenCV 3.3
+
+### Build
 
 Once we have installed all **dependencies** (see section below), we need to build the ```devel``` and ```build``` folders by typing inside the ```Autonomous_driving_Pipeline``` folder:
 
@@ -42,86 +147,14 @@ Once everything is set up and adjusted to our specific context, we are ready to 
 roslaunch loomo Loomo.launch
 ```
 
-### Structure
-
-We present all the different pillars of the pipeline and a brief explanation of each one.
-
-```
-Autonomous Pipeline
-│
-│─── Loomo ───> Package which contains the launch file with all configurable parameters.
-│
-│─── Message Types ───> Package which includes all types of messages for topics.
-│
-│─── Perception ───> Package and node to perceive and detect from cameras and depth sensors.
-│
-│─── Estimation ───> Package to estimate current state from sensors information.
-│       │
-│       │─── Robot State ───> Node to estimate the state from IMU and GPS.
-│       │
-│       └─── Map State ───> Node to generate a map and estimate the state from all sensors.
-│
-│─── Prediction ───> Package and node to generate future positions (motion) for detections.
-│
-│─── Path Planning ───> Package and node to calculate robot's desired path to avoid collision.
-│
-│─── Control ───> Package and node to ensure that the robot is following the desired path.
-│
-└─── Visualization ───> Plotting tools for all nodes current data.
-```
-
-### Dependencies
-
-**Python 3.7**
-
-If you do not have this Python version, we recommend you to download it by typing the following command in the Ubuntu terminal:
-
-```shell
-sudo chmod +x python_Autonomous_driving_pipeline.sh
-./python_Autonomous_driving_pipeline.sh
-```
-
-**ROS Kinetic/Melodic/Noetic**
-
-Depending on the Ubuntu version, there are three compatible ROS distributions.
-
-* Ubuntu 16.04: ROS Kinetic. http://wiki.ros.org/kinetic/Installation/Ubuntu
-
-* Ubuntu 18.04: ROS Melodic. http://wiki.ros.org/melodic/Installation/Ubuntu
-
-* Ubuntu 20.04: ROS Noetic. http://wiki.ros.org/noetic/Installation/Ubuntu
-
-**Python Libraries**
-
-We have built a bash script and a requirements.txt file with all the python modules required for our ROS pipeline. You can run them in your command line by typing:
-
-```shell
-sudo chmod +x dependencies_Autonomous_driving_pipeline.sh
-./dependencies_Autonomous_driving_pipeline.sh
-pip install -r requirements.txt
-```
-
-* Openpifpaf 0.11.9
-
-* TrajNet++
-
-* math
-
-* numpy
-
-* scipy
-
-* matplotlib
-
-* PIL
-
-* torch 1.7.1
-
-* collections
-
-* cython 0.29.21 
-
-* OpenCV 3.3
+## Future Improvements
+- [x] Add Perception functions from MMTrack
+- [x] Add more Documentation
+- [x] Redo the Install Procedure
+- [ ] Create Docker Image for easy install
+- [ ] Support for Multiple Person Tracking and Avoidance 
+- [ ] Create a simulation environment in gazebo to be able to test the algorithms without the robot
+- [ ] add a Robot description file (.urdf) of Loomo robot 
 
 
 
--- dependencies_Autonomous_driving_pipeline.sh
+++ dependencies_Autonomous_driving_pipeline.sh
@@ -81,68 +81,75 @@ pip install -U wstool
 pip install rosinstall rosdep update
 pip install pyyaml
 
-apt-get download libboost-all-dev libgtest-dev libpcl-dev libboost-thread-dev python3-rospkg libboost-filesystem-dev hddtemp python-rospkg python3-paramiko python-mock 
-apt-get download python-pyqt5.qtopengl libyaml-cpp-dev uuid-dev python3-empy libconsole-bridge-dev liburdfdom-dev liburdfdom-headers-dev libtinyxml-dev libtinyxml2-dev
+#download and install all deb packages
+mkdir deb
+cd deb
+apt-get download libboost-all-dev libgtest-dev libpcl-dev libboost-thread-dev python3-rospkg libboost-filesystem-dev hddtemp python3-paramiko python-mock 
+apt-get download libyaml-cpp-dev uuid-dev python3-empy libconsole-bridge-dev liburdfdom-dev liburdfdom-headers-dev libtinyxml-dev libtinyxml2-dev
 apt-get download python3-mock libgpgme-dev python-opengl libcppunit-dev python-wxtools pyqt5-dev python3-pyqt5 python3-pyqt5.qtsvg python3-sip-dev libboost-date-time-dev 
 apt-get download python3-pycryptodome python3-gnupg python3-rospkg-modules python3-catkin-pkg-modules python3-rosdep-modules python3-coverage python3-pydot python3-pygraphviz
-apt-get download gazebo9 libgazebo9-dev python3-defusedxml sbcl libpoco-dev libgtk2.0-dev libfltk1.3-dev libtool-bin python-pyqt5.qtwebkit python3-netifaces python-sip-dev libapr1-dev
-apt-get download libaprutil1-dev libboost-regex-dev libboost-system-dev liblog4cxx-dev libassimp-dev libogre-1.9-dev python3-catkin-pkg google-mock python3-nose python-catkin-pkg
+apt-get download gazebo9 libgazebo9-dev python3-defusedxml sbcl libpoco-dev libgtk2.0-dev libfltk1.3-dev libtool-bin python3-netifaces python-sip-dev libapr1-dev
+apt-get download libaprutil1-dev libboost-regex-dev libboost-system-dev liblog4cxx-dev libassimp-dev libogre-1.9-dev python3-catkin-pkg google-mock python3-nose
 
-dpkg -x gazebo9_9.0.0+dfsg5-3ubuntu1+ppa2_amd64.deb dir
-dpkg -x python-mock_2.0.0-3_all.deb dir
-dpkg -x google-mock_1.8.0-6_amd64.deb dir
-dpkg -x python-opengl_3.1.0+dfsg-1_all.deb dir
-dpkg -x hddtemp_0.3-beta15-53_amd64.deb dir                    
-dpkg -x python-pyqt5.qtopengl_5.10.1+dfsg-1ubuntu2_amd64.deb dir
-dpkg -x libapr1-dev_1.6.3-2_amd64.deb dir                      
-dpkg -x python-pyqt5.qtwebkit_5.10.1+dfsg-1ubuntu2_amd64.deb dir
-dpkg -x libaprutil1-dev_1.6.1-2_amd64.deb dir                  
-dpkg -x python-rospkg_1.2.9-100_all.deb dir
-dpkg -x libassimp-dev_4.1.0~dfsg-3_amd64.deb dir               
-dpkg -x python-sip-dev_4.19.7+dfsg-1ubuntu0.1_amd64.deb dir
-dpkg -x libboost-all-dev_1.65.1.0ubuntu1_amd64.deb dir         
-dpkg -x python-wxtools_3.0.2.0+dfsg-7_all.deb dir
-dpkg -x libboost-date-time-dev_1.65.1.0ubuntu1_amd64.deb dir   
-dpkg -x python3-catkin-pkg-modules_0.4.23-1_all.deb dir
-dpkg -x libboost-filesystem-dev_1.65.1.0ubuntu1_amd64.deb dir  
-dpkg -x python3-catkin-pkg_0.4.23-100_all.deb dir
-dpkg -x libboost-regex-dev_1.65.1.0ubuntu1_amd64.deb dir       
-dpkg -x python3-coverage_4.5+dfsg.1-3_amd64.deb dir
-dpkg -x libboost-system-dev_1.65.1.0ubuntu1_amd64.deb dir      
-dpkg -x python3-defusedxml_0.5.0-1ubuntu1_all.deb dir
-dpkg -x libboost-thread-dev_1.65.1.0ubuntu1_amd64.deb dir      
-dpkg -x python3-empy_3.3.2-1build1_all.deb dir
-dpkg -x libconsole-bridge-dev_0.4.0+dfsg-2_amd64.deb dir       
-dpkg -x python3-gnupg_0.4.1-1ubuntu1.18.04.1_all.deb dir
-dpkg -x libcppunit-dev_1.14.0-3_amd64.deb dir                  
-dpkg -x python3-mock_2.0.0-3_all.deb dir
-dpkg -x libfltk1.3-dev_1.3.4-6_amd64.deb dir                   
-dpkg -x python3-netifaces_0.10.4-0.1build4_amd64.deb dir
-dpkg -x libgazebo9-dev_9.0.0+dfsg5-3ubuntu1+ppa2_amd64.deb dir 
-dpkg -x python3-nose_1.3.7-3_all.deb dir
-dpkg -x libgpgme-dev_1.10.0-1ubuntu2_amd64.deb dir             
-dpkg -x python3-paramiko_2.0.0-1ubuntu1.2_all.deb dir
-dpkg -x libgtest-dev_1.8.0-6_amd64.deb dir                     
-dpkg -x python3-pycryptodome_3.4.7-1ubuntu1_amd64.deb dir
-dpkg -x libgtk2.0-dev_2.24.32-1ubuntu1_amd64.deb dir           
-dpkg -x python3-pydot_1.2.3-1_all.deb dir
-dpkg -x liblog4cxx-dev_0.10.0-13ubuntu2_amd64.deb dir          
-dpkg -x python3-pygraphviz_1.4~rc1-1build2.1_amd64.deb dir
-dpkg -x libogre-1.9-dev_1.9.0+dfsg1-10_amd64.deb dir           
-dpkg -x python3-pyqt5.qtsvg_5.10.1+dfsg-1ubuntu2_amd64.deb dir
-dpkg -x libpcl-dev_1.8.1+dfsg1-2ubuntu2.18.04.1_amd64.deb dir  
-dpkg -x python3-pyqt5_5.10.1+dfsg-1ubuntu2_amd64.deb dir
-dpkg -x libpoco-dev_1.8.0.1-1ubuntu4_amd64.deb dir             
-dpkg -x python3-rosdep-modules_0.20.0-1_all.deb dir
-dpkg -x libtinyxml-dev_2.6.2-4_amd64.deb dir                   
-dpkg -x python3-rospkg-modules_1.2.9-1_all.deb dir
-dpkg -x libtinyxml2-dev_6.0.0+dfsg-1_amd64.deb dir             
-dpkg -x python3-rospkg_1.2.9-100_all.deb dir
-dpkg -x libtool-bin_2.4.6-2_amd64.deb dir                      
-dpkg -x python3-sip-dev_4.19.7+dfsg-1ubuntu0.1_amd64.deb dir
-dpkg -x liburdfdom-dev_1.0.0-2ubuntu0.1_amd64.deb dir          
-dpkg -x sbcl_2%3a1.4.5-1_amd64.deb dir
-dpkg -x liburdfdom-headers-dev_1.0.0-1ubuntu0.1_amd64.deb dir
-dpkg -x libyaml-cpp-dev_0.5.2-4ubuntu1_amd64.deb dir           
-dpkg -x uuid-dev_2.31.1-0.4ubuntu3.7_amd64.deb dir
-dpkg -x python-catkin-pkg_0.4.23-100_all.deb dir
+for entry in ./*
+do
+    dpkg -x "$entry" ../dir
+done
+# dpkg -x gazebo9_9.0.0+dfsg5-3ubuntu1+ppa2_amd64.deb dir
+# dpkg -x python-mock_2.0.0-3_all.deb dir
+# dpkg -x google-mock_1.8.0-6_amd64.deb dir
+# dpkg -x python-opengl_3.1.0+dfsg-1_all.deb dir
+# dpkg -x hddtemp_0.3-beta15-53_amd64.deb dir                    
+# dpkg -x python-pyqt5.qtopengl_5.10.1+dfsg-1ubuntu2_amd64.deb dir
+# dpkg -x libapr1-dev_1.6.3-2_amd64.deb dir                      
+# dpkg -x python-pyqt5.qtwebkit_5.10.1+dfsg-1ubuntu2_amd64.deb dir
+# dpkg -x libaprutil1-dev_1.6.1-2_amd64.deb dir                  
+# dpkg -x python-rospkg_1.2.9-100_all.deb dir
+# dpkg -x libassimp-dev_4.1.0~dfsg-3_amd64.deb dir               
+# dpkg -x python-sip-dev_4.19.7+dfsg-1ubuntu0.1_amd64.deb dir
+# dpkg -x libboost-all-dev_1.65.1.0ubuntu1_amd64.deb dir         
+# dpkg -x python-wxtools_3.0.2.0+dfsg-7_all.deb dir
+# dpkg -x libboost-date-time-dev_1.65.1.0ubuntu1_amd64.deb dir   
+# dpkg -x python3-catkin-pkg-modules_0.4.23-1_all.deb dir
+# dpkg -x libboost-filesystem-dev_1.65.1.0ubuntu1_amd64.deb dir  
+# dpkg -x python3-catkin-pkg_0.4.23-100_all.deb dir
+# dpkg -x libboost-regex-dev_1.65.1.0ubuntu1_amd64.deb dir       
+# dpkg -x python3-coverage_4.5+dfsg.1-3_amd64.deb dir
+# dpkg -x libboost-system-dev_1.65.1.0ubuntu1_amd64.deb dir      
+# dpkg -x python3-defusedxml_0.5.0-1ubuntu1_all.deb dir
+# dpkg -x libboost-thread-dev_1.65.1.0ubuntu1_amd64.deb dir      
+# dpkg -x python3-empy_3.3.2-1build1_all.deb dir
+# dpkg -x libconsole-bridge-dev_0.4.0+dfsg-2_amd64.deb dir       
+# dpkg -x python3-gnupg_0.4.1-1ubuntu1.18.04.1_all.deb dir
+# dpkg -x libcppunit-dev_1.14.0-3_amd64.deb dir                  
+# dpkg -x python3-mock_2.0.0-3_all.deb dir
+# dpkg -x libfltk1.3-dev_1.3.4-6_amd64.deb dir                   
+# dpkg -x python3-netifaces_0.10.4-0.1build4_amd64.deb dir
+# dpkg -x libgazebo9-dev_9.0.0+dfsg5-3ubuntu1+ppa2_amd64.deb dir 
+# dpkg -x python3-nose_1.3.7-3_all.deb dir
+# dpkg -x libgpgme-dev_1.10.0-1ubuntu2_amd64.deb dir             
+# dpkg -x python3-paramiko_2.0.0-1ubuntu1.2_all.deb dir
+# dpkg -x libgtest-dev_1.8.0-6_amd64.deb dir                     
+# dpkg -x python3-pycryptodome_3.4.7-1ubuntu1_amd64.deb dir
+# dpkg -x libgtk2.0-dev_2.24.32-1ubuntu1_amd64.deb dir           
+# dpkg -x python3-pydot_1.2.3-1_all.deb dir
+# dpkg -x liblog4cxx-dev_0.10.0-13ubuntu2_amd64.deb dir          
+# dpkg -x python3-pygraphviz_1.4~rc1-1build2.1_amd64.deb dir
+# dpkg -x libogre-1.9-dev_1.9.0+dfsg1-10_amd64.deb dir           
+# dpkg -x python3-pyqt5.qtsvg_5.10.1+dfsg-1ubuntu2_amd64.deb dir
+# dpkg -x libpcl-dev_1.8.1+dfsg1-2ubuntu2.18.04.1_amd64.deb dir  
+# dpkg -x python3-pyqt5_5.10.1+dfsg-1ubuntu2_amd64.deb dir
+# dpkg -x libpoco-dev_1.8.0.1-1ubuntu4_amd64.deb dir             
+# dpkg -x python3-rosdep-modules_0.20.0-1_all.deb dir
+# dpkg -x libtinyxml-dev_2.6.2-4_amd64.deb dir                   
+# dpkg -x python3-rospkg-modules_1.2.9-1_all.deb dir
+# dpkg -x libtinyxml2-dev_6.0.0+dfsg-1_amd64.deb dir             
+# dpkg -x python3-rospkg_1.2.9-100_all.deb dir
+# dpkg -x libtool-bin_2.4.6-2_amd64.deb dir                      
+# dpkg -x python3-sip-dev_4.19.7+dfsg-1ubuntu0.1_amd64.deb dir
+# dpkg -x liburdfdom-dev_1.0.0-2ubuntu0.1_amd64.deb dir          
+# dpkg -x sbcl_2%3a1.4.5-1_amd64.deb dir
+# dpkg -x liburdfdom-headers-dev_1.0.0-1ubuntu0.1_amd64.deb dir
+# dpkg -x libyaml-cpp-dev_0.5.2-4ubuntu1_amd64.deb dir           
+# dpkg -x uuid-dev_2.31.1-0.4ubuntu3.7_amd64.deb dir
+# dpkg -x python-catkin-pkg_0.4.23-100_all.deb dir
